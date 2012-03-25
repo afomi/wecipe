588568d565c14ca38c11c442f22c5f0e19c17dca
Ryan Wold <wold@afomi.com>
2012-03-25 12:05:51 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wecipe::Application.config.secret_token = 'b1bcc6c34888b45a4cb18d9349d662f31aff0e107d2f639c78d2d86c75fe16daec0e3d4ce0beb2f7606d5fbf01f60c1cd4e6d614523cbdf98577327931db4145'
